9ccad42d6bc4ddd43019618a0599b4f17b525377
quocanh34 <phamquocanh2002ct@gmail.com>
2023-04-19 02:18:37 +0300

    add sun_command.sh

---
+++ run_command.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/kpu/kenlm.git
+cd kenlm
+sudo apt install build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev liblzma-dev
+./compile_query_only.sh
+cd ..
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+cd tokenizers/bindings/python
+pip install setuptools_rust
+python setup.py install
+cd ../../..
+pip install -r requirements.txt
+pip install datasets==2.11.0
