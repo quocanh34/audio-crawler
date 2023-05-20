73832e35bca8c2987b7f9ad73a2cd5b02662cb05
quocanh34 <phamquocanh2002ct@gmail.com>
2023-05-20 03:42:51 +0300

    update test

--- split.py
+++ split.py
@@ -10,19 +10,23 @@ def get_split(dataset, split, order, test_size):
     end = start + split_len if order != split else len(dataset['train']) 
     return dataset['train'].select(range(start, end)).train_test_split(test_size)
 
+# ds = load_dataset("linhtran92/final_dataset_500hrs_wer0")
 ds = load_dataset("quocanh34/youtube_dataset_locfuho")
 ds = ds.shuffle(seed=42)
 
-ds_1 = get_split(ds, split=3, order=1, test_size=0.05)
-ds_2 = get_split(ds, split=3, order=2, test_size=0.05)
-ds_3 = get_split(ds, split=3, order=3, test_size=0.05)
+ds_1 = get_split(ds, split=3, order=1, test_size=0.1)
+ds_2 = get_split(ds, split=3, order=2, test_size=0.1)
+ds_3 = get_split(ds, split=3, order=3, test_size=0.1)
 
 print(f"Total rows: {ds['train'].num_rows}")
 print(f"ds1: {ds_1}")
 print(f"ds2: {ds_2}")
 print(f"ds3: {ds_3}")
 
-ds_1.push_to_hub("quocanh34/viet_youtube_asr_v1", token="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")
+# ds_1.push_to_hub("quocanh34/viet_youtube_asr_corpus_v1", token="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")
+# ds_2.push_to_hub("linhtran92/viet_youtube_asr_corpus_v2", token='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')
+# ds_3.push_to_hub("quocanh34/viet_youtube_asr_corpus_v3", token="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")
+
 
 
 
